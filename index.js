a2b30623a858c564af6a8e18d54cab597cb7bc0d
Carlo Costa <carlo.costa92@gmail.com>
2025-03-25 14:28:14 +0100

    feat: set live reload and test in console

--- index.js
+++ index.js
@@ -7,4 +7,4 @@
 // Proviamo a modificare la nostra app Node, in modo da passare dal terminale una frase diversa da “Hello Boolean”. 
 // Forse potrà servirvi qualcosa vista ieri!
 
-console.log("hello world");
+console.log("hello fedrica");
